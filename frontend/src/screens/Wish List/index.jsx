cc640b5f35e2ee72c4219335f36c846ce059fa53
tsahiBarshevsky <tsahi.13@gmail.com>
2022-07-28 12:38:21 +0300

    Wishlist final design

--- frontend/src/screens/Wish List/index.jsx
+++ frontend/src/screens/Wish List/index.jsx
@@ -1,40 +1,60 @@
-import React from 'react';
-import { StyleSheet, Platform, StatusBar, Text, View, ScrollView } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Platform, StatusBar, Text, View, ScrollView, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 import MasonryList from '@react-native-seoul/masonry-list';
+import { useNavigation } from '@react-navigation/native';
 import { ProductCard } from '../../components';
+import { ThemeContext } from '../../utils/ThemeManager';
+import { darkMode, lightMode } from '../../utils/themes';
 
 const WishListScreen = () => {
+    const { theme } = useContext(ThemeContext);
     const wishList = useSelector(state => state.wishList);
+    const navigation = useNavigation();
 
     return (
-        <View style={styles.container}>
-            <ScrollView>
-                {wishList.products.length > 0 ?
-                    <>
-                        <Text>Wish list</Text>
-                        <MasonryList
-                            data={wishList.products}
-                            keyExtractor={(item) => item._id}
-                            renderItem={({ item, i }) => {
-                                return (
-                                    <ProductCard
-                                        product={item}
-                                        index={i}
-                                    />
-                                )
-                            }}
-                            contentContainerStyle={styles.contentContainerStyle}
-                            style={{ alignSelf: 'stretch' }}
-                        />
-                    </>
-                    :
-                    <Text>Wish list is empty</Text>
-                }
-            </ScrollView>
-        </View>
+        <SafeAreaView style={[styles.container, styles[`container${theme}`]]}>
+            {wishList.products.length > 0 ?
+                <ScrollView>
+                    <Text style={[styles.title, styles[`text${theme}`]]}>My Wishlist</Text>
+                    <MasonryList
+                        data={wishList.products}
+                        keyExtractor={(item) => item._id}
+                        renderItem={({ item, i }) => {
+                            return (
+                                <ProductCard
+                                    product={item}
+                                    index={i}
+                                />
+                            )
+                        }}
+                        contentContainerStyle={styles.contentContainerStyle}
+                        style={{ alignSelf: 'stretch' }}
+                    />
+                </ScrollView>
+                :
+                <View style={[styles.emptyWishlist, styles[`container${theme}`]]}>
+                    <Image
+                        source={require('../../../assets/Images/wish-list.png')}
+                        resizeMode='center'
+                        style={styles.image}
+                    />
+                    <Text style={[styles.caption, styles[`text${theme}`]]}>
+                        Your wishlist is empty. Browse the attractive Lego sets from Legoland!
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => navigation.goBack()}
+                        style={styles.emptyWishlistButton}
+                        activeOpacity={1}
+                    >
+                        <Text style={styles.emptyWishlistButtonCaption}>Start Shopping</Text>
+                    </TouchableOpacity>
+                </View>
+            }
+        </SafeAreaView>
     )
 }
+
 
 export default WishListScreen;
 
@@ -42,13 +62,57 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-        backgroundColor: '#f5f5f5',
-        // paddingHorizontal: 15
+    },
+    containerLight: {
+        backgroundColor: lightMode.background
+    },
+    containerDark: {
+        backgroundColor: darkMode.background
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        paddingHorizontal: 15
+    },
+    textLight: {
+        color: lightMode.text
+    },
+    textDark: {
+        color: darkMode.text
     },
     contentContainerStyle: {
-        // marginHorizontal: 15,
         marginBottom: 5,
         paddingHorizontal: 15,
         alignSelf: 'stretch'
+    },
+    caption: {
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    image: {
+        width: 250,
+        height: 250
+    },
+    emptyWishlist: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 25
+    },
+    emptyWishlistButton: {
+        backgroundColor: lightMode.primary,
+        borderRadius: 30,
+        paddingHorizontal: 35,
+        paddingVertical: 10,
+        position: 'absolute',
+        bottom: 30,
+        elevation: 1
+    },
+    emptyWishlistButtonCaption: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+        letterSpacing: 1
     }
 });
